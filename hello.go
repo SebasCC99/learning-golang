b9475b5d0979e198de4a8504fefef04659aafc6e
Sebastian Carazo <sebastian@lebas.dev>
2024-05-13 18:18:37 -0500

    refactor to accept language arg

--- hello.go
+++ hello.go
@@ -1,16 +1,38 @@
 package main
 
-import "fmt"
+const (
+	spanish = "Spanish"
+	french  = "French"
+	italian = "Italian"
 
-const englishHelloPrefix = "Hello, "
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+	italianHelloPrefix = "Ciao, "
+)
 
-func Hello(name string) string {
-	if name  == "" {
+func Hello(name string, language string) string {
+	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	case italian:
+		prefix = italianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	// fmt.Println(Hello("world", ""))
 }
